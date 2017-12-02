2743c468fa8400a4a1df3093baf7b4a4053b8925
Bradye1 <bradye1@vt.edu>
2017-12-02 14:07:48 -0500

    created the Database class but didn't add much. Also fixed Javadocs for Handle class

---
+++ src/Database.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+/**
+ * This class will be responsible for reading through the input file, creating
+ * the byte arrays, and handling methods that are called in the input file
+ * 
+ * @author Brady Engleman and Alex Bochel
+ * @version 12/2/2017
+ */
+public class Database
+{
+    
+    byte[] names;
+    byte[] artists;
+    
+    public Database(File name)
+    {
+        //idk what to put here yet
+        
+    }
+    
+    
+}
